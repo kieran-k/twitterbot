abb277fbeabfb18e27014001cf7f77e6a6c4d015
kk <58120049+kierankjk@users.noreply.github.com>
2021-02-28 14:52:02 -1000

    upload

---
+++ twitter.py
@@ -0,0 +1,53 @@
+import tweepy
+import time
+import re
+# import auth code files 
+import authcode
+
+# create seperate python file to hold authenitcation 
+# authcode.py contains codes for authentication 
+#auth.set_access_token( ** Codes go here **)
+
+#api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+#id = *Twitter account code here*
+
+file = open("twitcount.txt", "r")
+prev_count_str = (file.read())
+
+
+
+user = api.get_user(id)
+
+prev_fav_count = (int(prev_count_str))
+file.close
+
+fav = api.favorites(user) 
+
+fav_count = user.favourites_count
+
+calc = fav_count - prev_fav_count
+print(calc)
+
+for tweet in tweepy.Cursor(api.favorites, id=872727322138365953).items(calc): 
+    try: 
+        tweet.retweet()
+        print("retweeted")
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+
+prev_fav_count = fav_count
+
+file = open("twitcount.txt", "r")
+fav_count = (str(fav_count))
+file = open("twitcount.txt", "w")
+file.write(fav_count)
+file = open("twitcount.txt", "r")
+prev_count_str = (file.read())
+prev_fav_count = (int(prev_count_str))
+
+
